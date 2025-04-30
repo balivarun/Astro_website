d4baea8a42db9ff76984f58e59f16a7abb311f06
balivarun <varunbali345@gmail.com>
2025-04-30 19:25:36 +0530

    added a backgroud image to the home page

--- Astro/src/components/Home.tsx
+++ Astro/src/components/Home.tsx
@@ -5,11 +5,12 @@ const Home = () => {
 
   return (
     <div className='bg-orange-200 h-screen flex
-      flex-col items-center justify-center text-center'>
-       <div className='text-2xl font-semibold -translate-y-65 ' style={{ color: "#FF4433" }}>
+      flex-col items-center justify-center text-center bg-cover bg-center relative' style={{ backgroundImage: `url('/images/background3.webp')`, backgroundSize: 'cover' }}>
+       <div className="absolute inset-0 bg-white opacity-40"></div>
+       <div className='text-2xl font-semibold -translate-y-65 ' style={{ color: "#8B4000" }}>
         ।। ॐ नमो भगवते वासुदेवाय नम: ।।
       </div>
-      <div className="bg-orange-200 text-7xl font-extrabold -translate-y-100" style={{ color: "#FFA500" }}>
+      <div className=" text-7xl font-extrabold -translate-y-100" style={{ color: "#FF5F15" }}>
         Rahul Bali Astrology
       </div>
      
